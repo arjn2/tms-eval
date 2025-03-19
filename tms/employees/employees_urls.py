84c4286a6a5c01e4cc33109e5128a37145e2c882
arjn <arjunal9000@gmail.com>
2025-03-19 14:25:40 +0530

    Update employees_urls.py

--- tms/employees/employees_urls.py
+++ tms/employees/employees_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import handle_travel_request,create_travel_request
 
 urlpatterns = [
-    path('travel_request/', create_travel_request, name='travel_request_list_create'),
+    path('travel_request/', handle_travel_request, name='travel_request_list_create'),
     path('travel_request/<int:id>/', handle_travel_request, name='travel_request_detail'),
 ]
